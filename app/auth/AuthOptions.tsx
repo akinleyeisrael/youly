843a0ad9953182df88eca0c4122d7c9a3910c6a8
israel <akinleyeisrael12@gmail.com>
2024-01-22 06:41:18 -0500

    middlewra

--- app/auth/AuthOptions.tsx
+++ app/auth/AuthOptions.tsx
@@ -18,4 +18,7 @@ export const AuthOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt',
     },
+    pages: {
+        signIn: "/signin",
+    },
 };
--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ export { default } from 'next-auth/middleware';
 
 export const config = {
     matcher: [
-        // '/analytics/github',
+        '/analytics/github',
     ]
 }
