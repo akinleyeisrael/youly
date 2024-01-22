bbdfa01e6422443bf8c22c362d670f9533d33765
israel <akinleyeisrael12@gmail.com>
2024-01-21 19:50:47 -0500

    corrected authoption to another file

---
+++ app/auth/AuthOptions.tsx
@@ -0,0 +1,21 @@
+
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from "next-auth/providers/github";
+
+export const AuthOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: 'jwt',
+    },
+};
